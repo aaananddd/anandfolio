bfec027aca462ddbcb90dfdbe2898fab8cbc22e0
dracu-lah <nevilnicks4321@gmail.com>
2025-11-24 23:52:40 +0530

    fix:updated text

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,7 @@
 const Footer = () => {
-  return <footer className="bg-primary text-black">Iam Footer</footer>;
+  return (
+    <footer className="bg-primary text-center text-black">Iam Footer</footer>
+  );
 };
 
 export default Footer;
